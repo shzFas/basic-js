f60ba8fa7f545c1c7c76da08a8ee605016e48b6c
Yuriy Yurich <63981542+shzFas@users.noreply.github.com>
2022-10-12 17:10:46 +0600

    carbon dating done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  /* https://chem.libretexts.org/Bookshelves/Physical_and_Theoretical_Chemistry_Textbook_Maps/Supplemental_Modules_(Physical_and_Theoretical_Chemistry)/Nuclear_Chemistry/Nuclear_Kinetics/Half-Lives_and_Radioactive_Decay_Kinetics#section_2 */
+  /* c14 -> n14 + b- */
+  if (
+    typeof sampleActivity === 'string' &&
+    sampleActivity > 0 &&
+    sampleActivity <= MODERN_ACTIVITY
+) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) * HALF_LIFE_PERIOD / Math.LN2);
+}
+return false;
 }
 
 module.exports = {
